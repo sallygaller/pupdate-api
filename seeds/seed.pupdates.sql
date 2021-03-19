c83745854db6a99a6971d4b1e78108a73a538566
Sally Galler <sallyannegaller@gmail.com>
2021-03-19 15:40:34 -0700

    Changed seed files

--- seeds/seed.pupdates.sql
+++ seeds/seed.pupdates.sql
@@ -1,8 +1,8 @@
-INSERT INTO pupdates (date, starttime, endtime, location, locale, organizer)
+INSERT INTO pupdates (date, starttime, endtime, location, organizer)
 
 VALUES
-    ('2020/03/15', '09:30', '10:30', '7007 N Delaware Ave, Portland, OR 97217, USA', 'North Portland', 1),
-    ('2020/04/01', '10:30', '11:30', '6850 N Lombard St, Portland, OR 97203, USA', 'North Portland', 2),
-    ('2020/03/03', '13:30', '14:30', '9644 N Pier Park Pl, Portland, OR 97203, USA', 'North Portland', 3),
-    ('2020/02/28', '15:00', '16:30', 'N Princeton St & N Wall Ave, Portland, OR 97203, USA', 'North Portland', 4),
-    ('2020/03/05', '08:30', '10:00', '6850 N Lombard St, Portland, OR 97203, USA', 'North Portland', 2);
+    ('2020/03/15', '09:30', '10:30', '7007 N Delaware Ave, Portland, OR 97217, USA', 1),
+    ('2020/04/01', '10:30', '11:30', '6850 N Lombard St, Portland, OR 97203, USA', 2),
+    ('2020/03/03', '13:30', '14:30', '9644 N Pier Park Pl, Portland, OR 97203, USA', 3),
+    ('2020/02/28', '15:00', '16:30', 'N Princeton St & N Wall Ave, Portland, OR 97203, USA', 4),
+    ('2020/03/05', '08:30', '10:00', '6850 N Lombard St, Portland, OR 97203, USA', 2);
--- seeds/seed.pups.sql
+++ seeds/seed.pups.sql
@@ -1,8 +1,8 @@
 INSERT INTO pups (name, breed, mix, age, size, nervous, rambunctious, gentle, wrestling, walks, parks, foodobsessed, ballobsessed, description, owner)
 
 VALUES 
-('Eddie', 'dachshund', 'true', 'Adult', 'M', 'true', 'false', 'true', 'false', 'true', 'false', 'true', 'false', 'Eddie can be a bit shy around other dogs at first, but he is really sweet! He loves going on long walks where there is a lot to sniff', 1),
-('Buddy', 'beagle', 'true', 'Adult', 'L', 'false', 'true', 'false', 'true', 'false', 'true', 'true', 'true', 'Buddy can be a bit full on for some dogs, but he really just loves to play! He especially loves puppies', 2),
+('Eddie', 'dachshund', 'true', 'Adult', 'M', 'true', 'false', 'true', 'false', 'true', 'false', 'true', 'false', 'Eddie can be a bit shy around other dogs at first, but he is really sweet! He loves going on long walks where there is a lot to sniff.', 1),
+('Buddy', 'beagle', 'true', 'Adult', 'L', 'false', 'true', 'false', 'true', 'false', 'true', 'true', 'true', 'Buddy can be a bit full on for some dogs, but he really just loves to play! He especially loves puppies.', 2),
 ('Fran', 'pitbull', 'true', 'Adult', 'L', 'true', 'false', 'true', 'true', 'false', 'true', 'false', 'false', 'Fran loves to play, especially with other big dogs!', 2),
-('Rover', 'labrador', 'true', 'Adult', 'M', 'true', 'false', 'true', 'false', 'false', 'false', 'false', 'true', 'Rover could spend hours chasing his frisbee', 3),
-('Speckles', 'labrador', 'false', 'Senior', 'L', 'true', 'false', 'true', 'false', 'false', 'false', 'false', 'true', 'Speckles loves going on long, slow walks in the neighborhood', 4);
+('Rover', 'labrador', 'true', 'Adult', 'M', 'true', 'false', 'true', 'false', 'false', 'false', 'false', 'true', 'Rover could spend hours chasing his frisbee!', 3),
+('Speckles', 'labrador', 'false', 'Senior', 'L', 'true', 'false', 'true', 'false', 'false', 'false', 'false', 'true', 'Speckles loves going on long, slow walks in the neighborhood.', 4);
